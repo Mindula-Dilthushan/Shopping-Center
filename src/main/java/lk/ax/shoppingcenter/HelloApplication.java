f1a5de6794117b8c31f741ddfef2309c4fdb8a95
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2023-07-06 09:42:12 +0530

    update-main-class

--- src/main/java/lk/ax/shoppingcenter/HelloApplication.java
+++ src/main/java/lk/ax/shoppingcenter/HelloApplication.java
@@ -4,15 +4,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
+//        stage.initStyle(StageStyle.TRANSPARENT);
         stage.setScene(scene);
         stage.show();
     }
